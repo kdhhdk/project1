0efa9daac07dccc28fe238e6799f3e3d4a468d53
kim donghyun <kdhyun010619@gmail.com>
2022-05-11 21:32:23 +0900

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <input type="text" v-model="message">
+    {{message}}
+    <b-button variant="primary">Button</b-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  },
+  data () {
+    return {
+      message: '안녕'
+    }
+  }
+}
+</script>
